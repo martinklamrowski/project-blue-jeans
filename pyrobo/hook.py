3e28d9baeac1c876494674aa590edf06f0d1db24
TarunAK <tarun.kalikivaya@hotmail.com>
2021-03-08 11:14:42 -0500

    minor changes

--- pyrobo/hook.py
+++ pyrobo/hook.py
@@ -1,11 +1,12 @@
-
-
 class Hook():
-
     def __init__(self) -> None:
         self.lowered = True
         self.hooked = False
 
     def hook_object(self):
-        if (self.hooked):
-            self.lowered = False
+        self.hooked = True
+        self.lowered = False
+
+    def unhook_object(self):
+        self.hooked = False
+        self.lowered = True
